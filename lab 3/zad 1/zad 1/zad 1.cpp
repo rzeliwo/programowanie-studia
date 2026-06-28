7f5db3d44b4a28cf6fe10edbcca47d3bb1b9fec2
Adam <adam.pospiech@o2.pl>
2026-06-28 10:47:40 +0200

    zad 1

---
+++ lab 3/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char znak;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    cout << "Podaj znak dzialania (+, -, *, /): ";
+    cin >> znak;
+
+    switch (znak) {
+    case '+':
+        cout << "Wynik: " << a + b << endl;
+        break;
+    case '-':
+        cout << "Wynik: " << a - b << endl;
+        break;
+    case '*':
+        cout << "Wynik: " << a * b << endl;
+        break;
+    case '/':
+        if (b != 0)
+            cout << "Wynik: " << a / b << endl;
+        else
+            cout << "Blad: nie mozna dzielic przez zero!" << endl;
+        break;
+    default:
+        cout << "Blad: nieznany znak dzialania." << endl;
+    }
+
+    return 0;
+}
