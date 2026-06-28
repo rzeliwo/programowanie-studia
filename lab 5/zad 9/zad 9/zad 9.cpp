f93cb112474f2d34e5301b26486ddeeff84b38ae
Adam <adam.pospiech@o2.pl>
2026-06-28 11:14:50 +0200

    zad 9

---
+++ lab 5/zad 9/zad 9/zad 9.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            for (int k = 0; k <= 9; k++) {
+
+                if (i != j && i != k && j != k) {
+                    cout << i << j << k << endl;
+                    count++;
+                }
+            }
+        }
+    }
+
+    cout << "\nLiczb bez powtarzajacych sie cyfr: " << count << endl;
+
+    return 0;
+}
