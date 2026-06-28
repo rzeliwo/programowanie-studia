a6d46f6aac05b540c4e71fc898a9e43dc78f1ca4
Adam <adam.pospiech@o2.pl>
2026-06-28 10:56:36 +0200

    zad 1

---
+++ lab 4/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cout << "Podaj liczbe N: ";
+    cin >> N;
+
+    int i = 2;
+
+    cout << "\nLiczby parzyste od 2 do " << N << ":\n";
+
+    while (i <= N) {
+        cout << i << endl;
+        i += 2;
+    }
+
+    return 0;
+}
