bf7bf59283cf38646c0d2f513d527483433721c4
Adam <adam.pospiech@o2.pl>
2026-06-28 11:17:14 +0200

    zad 3

---
+++ lab 6/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int tab[10];
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Podaj liczbe nr " << i + 1 << ": ";
+        cin >> tab[i];
+    }
+
+    int minVal = tab[0];
+
+    for (int i = 1; i < 10; i++) {
+        if (tab[i] < minVal) {
+            minVal = tab[i];
+        }
+    }
+
+    cout << "Najmniejsza liczba to: " << minVal << endl;
+
+    return 0;
+}
