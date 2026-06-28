8dc5d5ebd3454fa3df14674aa9c57e8d47df0e5b
Adam <adam.pospiech@o2.pl>
2026-06-28 11:20:47 +0200

    zad 6

---
+++ lab 6/zad 6/zad 6/zad 6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int tab[3][3];
+    int suma = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Podaj liczbe dla [" << i + 1 << "][" << j + 1 << "]: ";
+            cin >> tab[i][j];
+            suma += tab[i][j];
+        }
+    }
+
+    cout << "Suma elementow macierzy: " << suma << endl;
+
+    return 0;
+}
