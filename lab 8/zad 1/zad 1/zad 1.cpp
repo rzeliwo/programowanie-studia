1bfc47ddec45484e39150ff2bc02adf086e9902f
Adam <adam.pospiech@o2.pl>
2026-06-28 11:26:22 +0200

    zad 1

---
+++ lab 8/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+bool czyParzysta(int n) {
+    return n % 2 == 0;
+}
+
+int main() {
+    int liczba;
+
+    cout << "Podaj liczbe calkowita: ";
+    cin >> liczba;
+
+    if (czyParzysta(liczba))
+        cout << "Liczba " << liczba << " jest parzysta." << endl;
+    else
+        cout << "Liczba " << liczba << " jest nieparzysta." << endl;
+
+    return 0;
+}
