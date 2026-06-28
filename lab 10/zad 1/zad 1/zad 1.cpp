6916913e2d67b0de0f94ad00a4a288f0409fead7
Adam <adam.pospiech@o2.pl>
2026-06-28 11:40:44 +0200

    zad 1

---
+++ lab 10/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 10;
+
+    int* pa = &a;
+    int* pb = &b;
+
+    cout << "Przed modyfikacja:\n";
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    *pa = 15;
+    *pb = 20;
+
+    cout << "\nPo modyfikacji:\n";
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
