0cabd60742268681ed1cb94423d2487a7b1e9c05
Adam <adam.pospiech@o2.pl>
2026-06-28 11:16:27 +0200

    zad 2

---
+++ lab 6/zad 2/zad 2/zad 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int tab[5];
+
+    cout << "Wprowadź 5 liczb całkowitych:\n";
+
+    for (int i = 0; i < 5; i++) {
+        cin >> tab[i];
+        if (tab[i] < 0) {
+            tab[i] = 0;
+        }
+    }
+
+    cout << "Liczby ujemne zamienione na 0: ";
+
+    for (int i = 0; i < 5; i++) {
+        cout << tab[i];
+        if (i < 4) cout << ", ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
