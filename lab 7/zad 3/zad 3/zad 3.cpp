8493b39b5925f78e08dd9b164422379ffb2ac4c2
Adam <adam.pospiech@o2.pl>
2026-06-28 11:24:39 +0200

    zad 3

---
+++ lab 7/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char tekst[100];
+    char znak;
+    int pozycje[100];
+    int licznik = 0;
+
+    cout << "Podaj ciag znakow: ";
+    cin.getline(tekst, 100);
+
+    cout << "Podaj znak do zliczenia: ";
+    cin >> znak;
+
+    for (int i = 0; tekst[i] != '\0'; i++) {
+        if (tekst[i] == znak) {
+            pozycje[licznik] = i + 1; // pozycje od 1
+            licznik++;
+        }
+    }
+
+    cout << "Liczba wystapien znaku '" << znak << "': " << licznik << endl;
+
+    cout << "Pozycje wystapien: ";
+    for (int i = 0; i < licznik; i++) {
+        cout << pozycje[i];
+        if (i < licznik - 1) cout << ", ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
