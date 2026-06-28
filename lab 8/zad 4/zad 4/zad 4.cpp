8e5cec20d913ad0293ce7bc28bc7e39111d8d6ac
Adam <adam.pospiech@o2.pl>
2026-06-28 11:33:00 +0200

    zad 4

---
+++ lab 8/zad 4/zad 4/zad 4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void GetArrayFromUser(int tab[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "Podaj liczbe nr " << i + 1 << ": ";
+        cin >> tab[i];
+    }
+}
+
+int FindMaxValue(int tab[], int size) {
+    int maxVal = tab[0];
+
+    for (int i = 1; i < size; i++) {
+        if (tab[i] > maxVal)
+            maxVal = tab[i];
+    }
+
+    return maxVal;
+}
+
+void DisplayResult(int maxVal) {
+    cout << "Najwieksza liczba: " << maxVal << endl;
+}
+
+int main() {
+    int tab[5];
+
+    GetArrayFromUser(tab, 5);
+    int maxVal = FindMaxValue(tab, 5);
+    DisplayResult(maxVal);
+
+    return 0;
+}
