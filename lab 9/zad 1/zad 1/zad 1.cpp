970adc929f3b43220217c7a9d148affa82254167
Adam <adam.pospiech@o2.pl>
2026-06-28 11:36:03 +0200

    zad 1

---
+++ lab 9/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int CalculateSumRecursive(int n) {
+    if (n == 1)
+        return 1;
+    return n + CalculateSumRecursive(n - 1);
+}
+
+int GetNumberFromUser() {
+    int n;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    return n;
+}
+
+void DisplayResult(int wynik, int n) {
+    cout << "Suma liczb od 1 do " << n << " wynosi: " << wynik << endl;
+}
+
+int main() {
+    int n = GetNumberFromUser();
+    int wynik = CalculateSumRecursive(n);
+    DisplayResult(wynik, n);
+
+    return 0;
+}
