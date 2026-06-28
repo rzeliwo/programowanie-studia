9ecd42d05a09b5ac8cebec3a1ef1b71704d0b641
Adam <adam.pospiech@o2.pl>
2026-06-28 10:48:43 +0200

    zad 2

---
+++ lab 3/zad 2/zad 2/zad 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double c;
+    char wybor;
+
+    cout << "Podaj temperature w stopniach Celsjusza: ";
+    cin >> c;
+
+    cout << "Wybierz jednostke docelowa (K - Kelvin, F - Fahrenheit): ";
+    cin >> wybor;
+
+    switch (wybor) {
+    case 'K':
+    case 'k':
+        cout << "Temperatura w Kelvinach: " << c + 273.15 << endl;
+        break;
+
+    case 'F':
+    case 'f':
+        cout << "Temperatura w stopniach Fahrenheita: " << (c * 9.0 / 5.0) + 32 << endl;
+        break;
+
+    default:
+        cout << "Blad: nieprawidlowa jednostka." << endl;
+    }
+
+    return 0;
+}
