e19243dbdd898079710ae97c045da433f85dfc79
Adam <adam.pospiech@o2.pl>
2026-06-28 11:24:19 +0200

    zad 2

---
+++ lab 7/zad 2/zad 2/zad 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char tekst[21];
+
+    cout << "Podaj ciag znakow (max 20 znakow): ";
+    cin.getline(tekst, 21);
+
+    for (int i = 0; tekst[i] != '\0'; i++) {
+        if (tekst[i] == 'a') {
+            tekst[i] = 'o';
+        }
+    }
+
+    cout << "Zmodyfikowany ciag znakow: " << tekst << endl;
+
+    return 0;
+}
