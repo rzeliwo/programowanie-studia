b2e2485c8366810b7f3b5ca14f0e41bd7ac3661d
Adam <adam.pospiech@o2.pl>
2026-06-28 09:42:26 +0200

    /

---
+++ lab1/zad2/zad2/zad2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    cout << "Liczba a: " << a << endl;
+    cout << "Liczba b: " << b << endl;
+    cout << "Suma: " << a + b << endl;
+    cout << "Różnica: " << a - b << endl;
+    cout << "Iloczyn: " << a * b << endl;
+
+    return 0;
+}
