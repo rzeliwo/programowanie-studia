1a2f0c73c1100a5604662fa45ddc168f4cbbe8d0
Adam <adam.pospiech@o2.pl>
2026-06-28 09:43:55 +0200

    zad3

---
+++ lab1/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 10;
+    float f = 3.14;
+    char c = 'A';
+    bool b = true;
+
+    cout << "Zmienna całkowita: " << i << endl;
+    cout << "Zmienna zmiennoprzecinkowa: " << f << endl;
+    cout << "Zmienna znakowa: " << c << endl;
+    cout << "Zmienna logiczna: " << b << endl;
+
+    return 0;
+}
