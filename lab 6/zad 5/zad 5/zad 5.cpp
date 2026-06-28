2cade4b47c1a9f37db1d5de95faeb4758273391f
Adam <adam.pospiech@o2.pl>
2026-06-28 11:19:55 +0200

    zad 5

---
+++ lab 6/zad 5/zad 5/zad 5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int tab[3][3];
+    int suma = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Podaj liczbe dla [" << i + 1 << "][" << j + 1 << "]: ";
+            cin >> tab[i][j];
+            suma += tab[i][j];
+        }
+    }
+
+    double srednia = suma / 9.0;
+
+    cout << "Srednia liczb: " << srednia << endl;
+
+    return 0;
+}
