df18033c2ce922d1c96b4ba2bbce7e95bb8923a9
Adam <adam.pospiech@o2.pl>
2026-06-28 11:03:28 +0200

    zad 2

---
+++ lab 4/zad 2/zad 2/zad 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cout << "Podaj liczbe N: ";
+    cin >> N;
+
+    int suma = 0;
+    int i = 1;
+
+    while (i <= N) {
+        suma += i;
+        i++;
+    }
+
+    cout << "\nSuma liczb naturalnych do " << N << ": " << suma << endl;
+
+    return 0;
+}
