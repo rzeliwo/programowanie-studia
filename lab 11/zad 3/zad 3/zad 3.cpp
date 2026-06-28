610bf2840243e54e04fc1f1f2d23956b447ba697
Adam <adam.pospiech@o2.pl>
2026-06-28 11:47:01 +0200

    zad 3

---
+++ lab 11/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Samochod {
+    string marka;
+    string model;
+    int rok_produkcji;
+};
+
+void WyswietlDane(Samochod s) {
+    cout << "Dane samochodu:\n";
+    cout << "Marka: " << s.marka << endl;
+    cout << "Model: " << s.model << endl;
+    cout << "Rok produkcji: " << s.rok_produkcji << endl;
+}
+
+int main() {
+    Samochod s1 = { "Toyota", "Corolla", 2018 };
+
+    WyswietlDane(s1);
+
+    return 0;
+}
