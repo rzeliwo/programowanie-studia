00a08589c6de7bcf5df93a57d634d70026e85e0f
Adam <adam.pospiech@o2.pl>
2026-06-28 10:41:29 +0200

    zad 1

---
+++ lab 2/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int punkty;
+
+    cout << "Podaj wynik egzaminu (liczba punktow): ";
+    cin >> punkty;
+
+    if (punkty < 0 || punkty > 100) {
+        cout << "Blad: liczba punktow musi byc w zakresie 0-100." << endl;
+    }
+    else if (punkty <= 49) {
+        cout << "Ocena: Niedostateczna" << endl;
+    }
+    else if (punkty <= 69) {
+        cout << "Ocena: Dostateczna" << endl;
+    }
+    else if (punkty <= 84) {
+        cout << "Ocena: Dobra" << endl;
+    }
+    else if (punkty <= 99) {
+        cout << "Ocena: Bardzo dobra" << endl;
+    }
+    else {
+        cout << "Ocena: Celujaca" << endl;
+    }
+
+    return 0;
+}
