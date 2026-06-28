f4e43018156f73f19d090ea83bcd6b5cfa810798
Adam <adam.pospiech@o2.pl>
2026-06-28 11:32:28 +0200

    zad 3

---
+++ lab 8/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string GetTextFromUser() {
+    string text;
+    getline(cin, text);
+    return text;
+}
+
+string ReverseString(string text) {
+    string rev = "";
+    for (int i = text.size() - 1; i >= 0; i--) {
+        rev += text[i];
+    }
+    return rev;
+}
+
+void DisplayResult(string text) {
+    cout << "Odwrocony ciag: " << text << endl;
+}
+
+int main() {
+    cout << "Podaj ciag znakow: ";
+    string text = GetTextFromUser();
+
+    string rev = ReverseString(text);
+    DisplayResult(rev);
+
+    return 0;
+}
