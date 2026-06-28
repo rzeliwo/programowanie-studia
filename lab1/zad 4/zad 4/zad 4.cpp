3207ab03519777d236f9c8268e753ccb09cc15e1
Adam <adam.pospiech@o2.pl>
2026-06-28 10:34:57 +0200

    zad 4

---
+++ lab1/zad 4/zad 4/zad 4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string imie;
+    int wiek;
+
+    cout << "Podaj swoje imię: ";
+    cin >> imie;
+
+    cout << "Podaj swój wiek: ";
+    cin >> wiek;
+
+    cout << "Witaj, " << imie << "! Masz " << wiek << " lat." << endl;
+
+    return 0;
+}
