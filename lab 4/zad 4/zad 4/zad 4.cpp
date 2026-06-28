b0bd55998bbfeccea6142821d22c7e582114dd4e
Adam <adam.pospiech@o2.pl>
2026-06-28 11:05:45 +0200

    zad 4

---
+++ lab 4/zad 4/zad 4/zad 4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cout << "Podaj liczbe calkowita dodatnia N: ";
+    cin >> N;
+
+    int i = 1;
+    int licznik = 0;
+
+    if (N < 1) {
+        cout << "Blad: N musi byc dodatnie." << endl;
+        return 0;
+    }
+
+    do {
+        if (i % 2 == 0) {
+            licznik++;
+        }
+        i++;
+    } while (i <= N);
+
+    cout << "Liczba liczb parzystych od 1 do " << N << " wynosi: " << licznik << endl;
+
+    return 0;
+}
