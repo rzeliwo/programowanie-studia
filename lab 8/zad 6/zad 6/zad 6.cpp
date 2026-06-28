e56ebe4b8bc6cff77a13716f4d2771f3dbc5be97
Adam <adam.pospiech@o2.pl>
2026-06-28 11:34:20 +0200

    zad 6

---
+++ lab 8/zad 6/zad 6/zad 6.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int GetNumberFromUser() {
+    int n;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    return n;
+}
+
+long long CalculateFactorial(int n) {
+    long long wynik = 1;
+
+    for (int i = 1; i <= n; i++) {
+        wynik *= i;
+    }
+
+    return wynik;
+}
+
+void DisplayResult(int n, long long wynik) {
+    cout << "Silnia liczby " << n << " wynosi: " << wynik << endl;
+}
+
+int main() {
+    int n = GetNumberFromUser();
+    long long wynik = CalculateFactorial(n);
+    DisplayResult(n, wynik);
+
+    return 0;
+}
