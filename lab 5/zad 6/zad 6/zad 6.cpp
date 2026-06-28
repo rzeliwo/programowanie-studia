51556cf69dbf8a3224cfda16aed04b8b07105d3d
Adam <adam.pospiech@o2.pl>
2026-06-28 11:13:24 +0200

    zad 6

---
+++ lab 5/zad 6/zad 6/zad 6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int w, h;
+
+    cout << "Podaj szerokosc: ";
+    cin >> w;
+    cout << "Podaj wysokosc: ";
+    cin >> h;
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (i == 0 || i == h - 1 || j == 0 || j == w - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
