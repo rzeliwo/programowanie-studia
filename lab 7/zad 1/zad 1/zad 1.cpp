5d04b71a577caacb6811f69f20a3942d407566f8
Adam <adam.pospiech@o2.pl>
2026-06-28 11:22:54 +0200

    zad 1

---
+++ lab 7/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char tekst[21];
+
+    cout << "Podaj ciag znakow (max 20 znakow): ";
+    cin.getline(tekst, 21);
+
+    cout << "Wprowadzony ciag znakow: " << tekst << endl;
+
+    return 0;
+}
