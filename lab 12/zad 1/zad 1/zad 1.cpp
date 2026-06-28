0e2eb2fab2f4eac29488839370c865b6da567312
Adam <adam.pospiech@o2.pl>
2026-06-28 11:47:31 +0200

    zad 1

---
+++ lab 12/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream plik("dane.txt");
+
+    if (!plik) {
+        cout << "Nie mozna otworzyc pliku!" << endl;
+        return 1;
+    }
+
+    string imie, nazwisko;
+    int wiek;
+
+    while (plik >> imie >> nazwisko >> wiek) {
+        cout << "Imie: " << imie
+            << ", Nazwisko: " << nazwisko
+            << ", Wiek: " << wiek << endl;
+    }
+
+    plik.close();
+    return 0;
+}
