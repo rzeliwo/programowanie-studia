42e82385c292cce174ddf8b0c0124be2a615f6db
Adam <adam.pospiech@o2.pl>
2026-06-28 10:36:13 +0200

    zad 5

---
+++ lab1/zad 5/zad 5/zad 5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Podaj pierwszą liczbę: ";
+    cin >> a;
+
+    cout << "Podaj drugą liczbę: ";
+    cin >> b;
+
+    cout << "Suma: " << a + b << endl;
+    cout << "Różnica: " << a - b << endl;
+    cout << "Iloczyn: " << a * b << endl;
+
+    if (b != 0) {
+        cout << "Iloraz: " << a / b << endl;
+        cout << "Reszta z dzielenia: " << a % b << endl;
+    }
+    else {
+        cout << "Nie można dzielić przez zero!" << endl;
+    }
+
+    return 0;
+}
