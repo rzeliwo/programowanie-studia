97bbc641920ec5fa54d793a8f015775bdb2d48d2
Adam <adam.pospiech@o2.pl>
2026-06-28 11:14:20 +0200

    zad 8

---
+++ lab 5/zad 8/zad 8/zad 8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int w, h;
+
+    cout << "Podaj szerokosc: ";
+    cin >> w;
+    cout << "Podaj wysokosc: ";
+    cin >> h;
+
+    for (int i = 0; i < h; i++) {
+        for (int s = 0; s < i; s++)
+            cout << " ";
+
+        for (int j = 0; j < w - 2 * i; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
