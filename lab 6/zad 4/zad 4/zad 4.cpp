ee7fef42f6f3c39fa22168149e76e8cfab35957a
Adam <adam.pospiech@o2.pl>
2026-06-28 11:18:53 +0200

    zad 4

---
+++ lab 6/zad 4/zad 4/zad 4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int tab[10];
+    int suma = 0;
+
+    cout << "Podaj 10 liczb:\n";
+
+    for (int i = 0; i < 10; i++) {
+        cin >> tab[i];
+    }
+
+    cout << "Liczby parzyste: ";
+
+    for (int i = 0; i < 10; i++) {
+        if (tab[i] % 2 == 0) {
+            cout << tab[i] << ", ";
+            suma += tab[i];
+        }
+    }
+
+    cout << "\nSuma liczb parzystych: " << suma << endl;
+
+    return 0;
+}
