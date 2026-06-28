a2c90d1975dcdc7d598ffea56d193722032c4744
Adam <adam.pospiech@o2.pl>
2026-06-28 11:37:26 +0200

    zad 2

---
+++ lab 9/zad 2/zad 2/zad 2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int Fibonacci(int n) {
+    if (n == 1 || n == 2)
+        return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int GetNumberFromUser() {
+    int n;
+    cout << "Podaj liczbe elementow: ";
+    cin >> n;
+    return n;
+}
+
+void DisplayFibonacciSequence(int n) {
+    cout << "Ciag Fibonacciego: ";
+
+    for (int i = 1; i <= n; i++) {
+        cout << Fibonacci(i);
+        if (i < n) cout << ", ";
+    }
+
+    cout << endl;
+}
+
+int main() {
+    int n = GetNumberFromUser();
+    DisplayFibonacciSequence(n);
+
+    return 0;
+}
