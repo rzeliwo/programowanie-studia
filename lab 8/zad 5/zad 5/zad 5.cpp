c8a5b60e312a957afaaaff1e1386e9a5c00ee0d9
Adam <adam.pospiech@o2.pl>
2026-06-28 11:33:30 +0200

    zad 5

---
+++ lab 8/zad 5/zad 5/zad 5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void DuzaMala(int liczba, int wylosowana) {
+    if (liczba < wylosowana)
+        cout << "Za mało\n";
+    else if (liczba > wylosowana)
+        cout << "Za dużo\n";
+}
+
+bool CheckLiczba(int liczba, int wylosowana) {
+    return liczba == wylosowana;
+}
+
+int main() {
+    srand(time(NULL));
+    int wylosowana = rand() % 100 + 1;
+
+    int liczba;
+
+    do {
+        cout << "Podaj liczbe: ";
+        cin >> liczba;
+
+        if (CheckLiczba(liczba, wylosowana)) {
+            cout << "Gratulacje! Odgadles liczbe.\n";
+            break;
+        }
+
+        DuzaMala(liczba, wylosowana);
+
+    } while (true);
+
+    return 0;
+}
