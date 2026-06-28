ad7b1d292f2ffe488912ae0933d0c0ad36768619
Adam <adam.pospiech@o2.pl>
2026-06-28 11:25:33 +0200

    zad 4

---
+++ lab 7/zad 4/zad 4/zad 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char tekst[100];
+    char odwrocony[100];
+
+    cout << "Podaj ciag znakow: ";
+    cin.getline(tekst, 100);
+
+    int dlugosc = strlen(tekst);
+
+    for (int i = 0; i < dlugosc; i++) {
+        odwrocony[i] = tekst[dlugosc - i - 1];
+    }
+
+    odwrocony[dlugosc] = '\0';
+
+    cout << "Odwrocony ciag: " << odwrocony << endl;
+
+    return 0;
+}
