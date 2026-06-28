f0b8f44172bad9f3caab238be674f52dc3f9b1af
Adam <adam.pospiech@o2.pl>
2026-06-28 11:11:22 +0200

    zad 4

---
+++ lab 5/zad 4/zad 4/zad 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int w, k;
+    cout << "Podaj liczbe wierszy: ";
+    cin >> w;
+    cout << "Podaj liczbe kolumn: ";
+    cin >> k;
+
+    int liczba = 1;
+
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < k; j++) {
+            cout << liczba << "\t";
+            liczba += 2;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
