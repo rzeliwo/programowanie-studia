eabd37ca2e672be587f07d777ac45fc9cd449fe8
Adam <adam.pospiech@o2.pl>
2026-06-28 11:04:20 +0200

    zad 3

---
+++ lab 4/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int liczba;
+    char wybor;
+
+    do {
+        cout << "Podaj liczbe: ";
+        cin >> liczba;
+
+        cout << "Wprowadziles liczbe: " << liczba << endl;
+
+        cout << "Czy chcesz wprowadzic kolejna liczbe? (t/n): ";
+        cin >> wybor;
+
+    } while (wybor == 't' || wybor == 'T');
+
+    cout << "Dziekuje za wprowadzenie liczb!" << endl;
+
+    return 0;
+}
