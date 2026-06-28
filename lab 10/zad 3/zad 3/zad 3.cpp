7b9720ecf42c9be76ad563259906e91b67bb19cd
Adam <adam.pospiech@o2.pl>
2026-06-28 11:42:38 +0200

    zad 3

---
+++ lab 10/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int ObliczSume(int* tab, int size) {
+    int suma = 0;
+    for (int i = 0; i < size; i++) {
+        suma += *(tab + i);
+    }
+    return suma;
+}
+
+int main() {
+    int tab[4] = { 5, 10, 15, 20 };
+
+    cout << "Suma elementow tablicy: " << ObliczSume(tab, 4) << endl;
+
+    for (int i = 0; i < 4; i++) {
+        cout << "Podaj nowa wartosc dla elementu " << i << ": ";
+        cin >> *(tab + i);
+    }
+
+    cout << "Nowa tablica:\n";
+    for (int i = 0; i < 4; i++) {
+        cout << *(tab + i) << " ";
+    }
+
+    cout << "\nNowa suma elementow tablicy: " << ObliczSume(tab, 4) << endl;
+
+    return 0;
+}
