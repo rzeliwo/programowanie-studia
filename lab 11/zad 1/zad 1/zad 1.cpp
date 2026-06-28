cbfed0766a2bea5b93eee6b81fde32bb2115042d
Adam <adam.pospiech@o2.pl>
2026-06-28 11:45:54 +0200

    zad 1

---
+++ lab 11/zad 1/zad 1/zad 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Osoba {
+    string imie;
+    string nazwisko;
+    int wiek;
+};
+
+int main() {
+    Osoba o1 = { "Jan", "Kowalski", 25 };
+
+    cout << "Dane przed modyfikacja:\n";
+    cout << "Imie: " << o1.imie
+        << ", Nazwisko: " << o1.nazwisko
+        << ", Wiek: " << o1.wiek << endl;
+
+    o1.wiek = 30;
+
+    cout << "\nDane po modyfikacji:\n";
+    cout << "Imie: " << o1.imie
+        << ", Nazwisko: " << o1.nazwisko
+        << ", Wiek: " << o1.wiek << endl;
+
+    return 0;
+}
