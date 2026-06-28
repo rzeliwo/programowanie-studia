decfb79de7c30c50243b76dbc755e4e259cbc19b
Adam <adam.pospiech@o2.pl>
2026-06-28 11:10:46 +0200

    zad 3

---
+++ lab 5/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Podaj liczbe N: ";
+    cin >> N;
+
+    cout << "\nLiczby nieparzyste od 1 do " << N << ":\n";
+
+    for (int i = 1; i <= N; i++) {
+        if (i % 2 != 0)
+            cout << i << endl;
+    }
+
+    return 0;
+}
