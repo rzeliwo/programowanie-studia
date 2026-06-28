2a2bc8bf51e6051fbe3dd68fc224faae21efa7c1
Adam <adam.pospiech@o2.pl>
2026-06-28 11:39:38 +0200

    zad 3

---
+++ lab 9/zad 3/zad 3/zad 3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int CalculatePowerRecursive(int a, int b) {
+    if (b == 0)
+        return 1;
+    return a * CalculatePowerRecursive(a, b - 1);
+}
+
+void DisplayResult(int a, int b, int wynik) {
+    cout << a << " do potegi " << b << " wynosi: " << wynik << endl;
+}
+
+int main() {
+    int a, b;
+
+    cout << "Podaj liczbe podstawowa: ";
+    cin >> a;
+
+    cout << "Podaj wykladnik: ";
+    cin >> b;
+
+    int wynik = CalculatePowerRecursive(a, b);
+    DisplayResult(a, b, wynik);
+
+    return 0;
+}
