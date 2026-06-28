64f55cd9d3ed86bf2e5f9bd7a45ad045d1f1821f
Adam <adam.pospiech@o2.pl>
2026-06-28 10:37:52 +0200

    zad 6

---
+++ lab1/zad 6/zad 6/zad 6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Podaj pierwszą liczbę: ";
+    cin >> a;
+
+    cout << "Podaj drugą liczbę: ";
+    cin >> b;
+
+    int suma = a + b;
+
+    cout << "Suma: " << suma << endl;
+
+    if (suma % 2 == 0) {
+        cout << "Suma jest liczbą parzystą." << endl;
+    }
+    else {
+        cout << "Suma jest liczbą nieparzystą." << endl;
+    }
+
+    return 0;
+}
